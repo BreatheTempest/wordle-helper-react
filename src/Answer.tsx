32a60ec8ac9e49ad4e026de8d09ff168804c749d
BreatheTempest <zabulonov.a@gmail.com>
2022-11-04 22:20:26 +0200

    add ability to add/remove letters, change rows

--- src/Answer.tsx
+++ src/Answer.tsx
@@ -8,7 +8,7 @@ export default function Answer({ array }: AnswerProps) {
 
   return <div className="grid grid-cols-5 text-center gap-2">
     {Array(5).fill('').map((box, i) =>
-      (<div key={i} className="border-2 border-slate-600 flex items-center justify-center text-3xl">{array[i]?.toUpperCase()}</div>)
+      (<div key={i} className="border-2 border-slate-600 flex items-center justify-center text-3xl font-bold">{array[i]?.toUpperCase()}</div>)
     )}
   </div>;
 }
--- src/App.tsx
+++ src/App.tsx
@@ -14,26 +14,31 @@ function App() {
     const button = e.target as HTMLButtonElement;
     switch (button.name) {
       case 'enter':
-        setRowIndex(prev => prev + 1)
-        console.log('hello')
+        if (inputLetters[rowIndex].length === 5)
+          setRowIndex(prev => prev + 1)
         break
       case 'delete':
-        console.log('bye')
-        break
-      default:
-        console.log(button.name)
         setInputLetters(prev => {
           return prev.map((row, index) => {
-            if (index === rowIndex) return [...row, button.name]
+            if (index === rowIndex) return [...row.slice(0, row.length - 1)]
+            else return row
+          })
+        })
+        break
+      default:
+        setInputLetters(prev => {
+          return prev.map((row, index) => {
+            if (index === rowIndex && row.length < 5) return [...row, button.name]
             else return row
           })
         })
     }
   }
 
+
   return (
     <main className="h-screen bg-black flex flex-col items-center text-gray-100 font-open-sans">
-      <nav className="font-patua text-4xl">Wordle</nav>
+      <nav className="font-patua text-4xl mt-4">Wordle</nav>
       <div className='mt-8 w-80 h-96 grid grid-rows-6 gap-2'>
         {inputLetters.map((array, index) => <Answer key={index} array={array} />)}
       </div>
